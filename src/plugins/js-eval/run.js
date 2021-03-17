e063be28a2123c3b64d0131b01a90524ac486afb
Cyril Auburtin <cyril.auburtin@gmail.com>
2021-03-17 19:51:18 +0100

    js-eval: set timers.promises is defined

--- src/plugins/js-eval/run.js
+++ src/plugins/js-eval/run.js
@@ -1,8 +1,11 @@
 const { Script, SourceTextModule, createContext } = require('vm');
 const util = require('util');
-const builtinModules = require('module').builtinModules.filter((a) => !/^_|\//.test(a));
+const builtinModules = require('module').builtinModules.filter(
+  (s) => !/^_|\//.test(s),
+);
+const { setTimeout } = require('timers/promises');
 
-// copied from https://github.com/devsnek/docker-js-eval/run.js
+// code taken from https://github.com/devsnek/docker-js-eval/run.js
 
 const inspect = (val) => {
   try {
@@ -10,89 +13,150 @@ const inspect = (val) => {
       maxArrayLength: 20,
       breakLength: Infinity,
       colors: false,
+      compact: true,
+      depth: 10,
     });
   } catch {
     return '';
   }
 };
 
-const run = async (code, environment, timeout) => {
-  if (environment === 'node-cjs') {
-    const script = new Script(code);
-    global.module = module;
-    global.require = require;
-    global.exports = exports;
-    global.__dirname = __dirname;
-    global.__filename = __filename;
-    for (const name of builtinModules) {
-      const setReal = (val) => {
-        delete global[name];
-        global[name] = val;
-      };
+function exposeBuiltinInGlobal(name, value = ) {
+  const setReal = (val) => {
+    delete global[name];
+    global[name] = val;
+  };
+  Object.defineProperty(global, name, {
+    get: () => {
+      const value = require(name); // eslint-disable-line
+      if (name === 'timers') {
+        value.promises = value.promises || require('timers/promises'); // eslint-disable-line
+      }
+      delete global[name];
       Object.defineProperty(global, name, {
-        get: () => {
-          const lib = require(name); // eslint-disable-line
-          delete global[name];
-          Object.defineProperty(global, name, {
-            get: () => lib,
-            set: setReal,
-            configurable: true,
-            enumerable: false,
-          });
-          return lib;
-        },
+        get: () => value,
         set: setReal,
         configurable: true,
         enumerable: false,
       });
+      return value;
+    },
+    set: setReal,
+    configurable: true,
+    enumerable: false,
+  });
+}
+
+async function run(code, environment, timeout) {
+  switch (environment) {
+    case 'node-cjs': {
+      const script = new Script(code);
+      global.module = module;
+      global.require = require;
+      global.exports = exports;
+      global.__dirname = __dirname;
+      global.__filename = __filename;
+      builtinModules.forEach(exposeBuiltinInGlobal);
+      const result = await script.runInThisContext({
+        timeout,
+        displayErrors: true,
+      });
+      return inspect(result);
     }
-    return script.runInThisContext({
-      timeout,
-      displayErrors: true,
-    });
-  }
-  if (environment === 'module') {
-    const module = new SourceTextModule(code, {
-      context: createContext(Object.create(null)),
-    });
-    await module.link(async () => {
-      throw new Error('Unable to resolve import');
-    });
-    module.instantiate();
-    const { result } = await module.evaluate({ timeout });
-    return result;
-  }
-  if (environment === 'script') {
-    const script = new Script(code, {
-      displayErrors: true,
-    });
-    return script.runInContext(createContext(Object.create(null)), {
-      timeout,
-      displayErrors: true,
-    });
-  }
 
-  throw new RangeError(`Invalid environment: ${environment}`);
-};
+    case 'module': {
+      const module = new SourceTextModule(code, {
+        context: createContext(Object.create(null)),
+      });
+      await module.link(async () => {
+        throw new Error('Unable to resolve import');
+      });
 
-if (!module.parent) {
+      const timeoutCtrl = new AbortController();
+      const result = await Promise.race([
+        module.evaluate({ timeout }).finally(() => timeoutCtrl.abort()),
+        setTimeout(Math.floor(timeout * 1.5), timeoutCtrl).then(() => {
+          throw new Error('The execution timed out');
+        }),
+      ]);
+
+      return inspect(result);
+    }
+
+    case 'script': {
+      const script = new Script(code, {
+        displayErrors: true,
+      });
+      const result = await script.runInContext(createContext(Object.create(null)), {
+        timeout,
+        displayErrors: true,
+      });
+      return inspect(result);
+    }
+
+    case 'engine262': {
+      const {
+        Agent,
+        ManagedRealm,
+        Value,
+        CreateDataProperty,
+        FEATURES,
+        setSurroundingAgent,
+        inspect: _inspect,
+      } = require('engine262'); // eslint-disable-line
+
+      const agent = new Agent({
+        features: FEATURES.map((o) => o.name),
+      });
+      setSurroundingAgent(agent);
+
+      const realm = new ManagedRealm();
+
+      return new Promise((resolve, reject) => {
+        realm.scope(() => {
+          const print = new Value((args) => {
+            console.log(...args.map((tmp) => _inspect(tmp)));
+            return Value.undefined;
+          });
+          CreateDataProperty(realm.GlobalObject, new Value('print'), print);
+
+          const completion = realm.evaluateScript(code);
+          if (completion.Type === 'throw') {
+            reject(_inspect(completion.Value));
+          } else {
+            resolve(_inspect(completion.Value));
+          }
+        });
+      });
+    }
+
+    default:
+      throw new RangeError(`Invalid environment: ${environment}`);
+  }
+}
+
+if (require.main === module) {
   (async () => {
     let code = process.argv[2];
-    if (!code) { // if no argument, read from stdin
+    if (!code) {
+      // if no argument, read from stdin
       code = '';
       for await (const chunk of process.stdin) {
         code += chunk;
       }
     }
     try {
-      const result = await run(
+      const output = await run(
         code,
         process.env.JSEVAL_ENV,
         Number.parseInt(process.env.JSEVAL_TIMEOUT, 10) || undefined,
       );
-      process.stdout.write(inspect(result));
+      process.stdout.write('⬊ ');
+      process.stdout.write(output);
     } catch (error) {
-      process.stdout.write(error instanceof Error ? `${error.name}: ${error.message}` : `${error}`);
+      process.stdout.write(
+        error instanceof Error ? `${error.name}: ${error.message}` : `${error}`,
+      );
       process.exit(1);
     }
   })();
